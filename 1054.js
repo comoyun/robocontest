98528fec34313fe53b1c6abe33ceece4cdc0e9b9
Khumoyun <contact@xumoyun.uz>
2025-10-25 19:12:50 +0500

    fix edge case

--- 1054.js
+++ 1054.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 const n = fs.readFileSync(0, 'utf8');
 
 /* fuck robocontest */
-console.log([1, n, 1].join(' '));
+if (n == 1) console.log(-1);
+else console.log([1, n, 1].join(' '));
