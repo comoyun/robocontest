05c87d7d5ad6795e6a663e50c0e41dd163aaff9d
Khumoyun <contact@xumoyun.uz>
2025-10-27 21:24:57 +0500

    add solution

---
+++ 0562.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+const [m, n] = lines[0].split(' ').map(Number);
+const stones = lines[1].split(' ').map(Number);
+
+/* 
+
+toshlarni kamayish tartibida sortirovka qilsak kamida nechta bola
+tosh terishini ey yo'ge olib kelishini topamiz
+
+*/
+
+stones.sort((a, b) => b - a);
+
+let count = 0;
+let result = -1;
+
+for (let i = 0; i < n; i++) {
+    count += stones[i];
+    if (count >= m) {
+        result = i + 1;
+        break;
+    }
+}
+
+console.log(result);
