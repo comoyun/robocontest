602075c5bad5b2fdc27ba867a1f65bd676f7fe7c
Khumoyun <contact@xumoyun.uz>
2025-11-10 22:53:14 +0500

    add explanation

--- 0229.js
+++ 0229.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const [a, b] = fs.readFileSync(0, 'utf8').trim().split(' ').map(BigInt);
 
+// o'rta arifmetik (a + b) / 2
+// o'rta geometrik sqrt(a * b)
+
+// son juda katta (10 ** 18), shuning uchun ildiz olish noqulay
+// lekin agar ikkala tomonni ham kvadratga ko'tarsak, ildiz yo'qoladi va ishimiz ossonlashadi
+
 const m1 = (a + b) * (a + b);
 const m2 = 4n * a * b;
 
