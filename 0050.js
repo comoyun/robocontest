c0ed25ba07fc235b23110e5edf477f40b18c4023
Khumoyun <contact@xumoyun.uz>
2025-11-27 00:45:03 +0500

    add explanations

--- 0050.js
+++ 0050.js
@@ -3,8 +3,12 @@ const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
 const result = [];
 
 for (let i = 1; i < lines.length; i++) {
+    // 2ta sonni qatordan chiqarib olish
     const [n, m] = lines[i].split(' ').map(Number);
+
+    // 1dan n-gacha bo'lgan sonlar yig'indisini hisoblash
     const out = (n * (n + 1)) / 2;
+
     if (out === m) result.push(1);
     else result.push(0);
 }
