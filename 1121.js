07af5bf779b2ac504d62bba862bc524448fc70e1
Khumoyun <contact@xumoyun.uz>
2025-12-06 22:24:53 +0500

    add explanation

--- 1121.js
+++ 1121.js
@@ -3,5 +3,7 @@ const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
 
 for (let i = 1; i < lines.length; i++) {
     const n = lines[i];
+
+    // n toq bo'lsa (oxirgi biti 1), 0 chiqaradi aks holda 1
     console.log(n & 1 ? 0 : 1);
 }
