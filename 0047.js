8a0c336d148ffa836f3f2a50091508b168b0876c
Khumoyun <contact@xumoyun.uz>
2025-12-06 22:23:23 +0500

    add explanation

--- 0047.js
+++ 0047.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
 
-for (let i = 1; i < lines.length; i++)
+for (let i = 1; i < lines.length; i++) {
+    // 2lik sanoq sistemasiga o'girib, 1-larni sanaydi
+    // -1, chunki split('1') length 1 ortiq bo'ladi
     console.log((+lines[i]).toString(2).split('1').length - 1);
+}
