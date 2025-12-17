02b3e4ed7a5c0e1e75b39a97a7649d08e2779a5c
Khumoyun <contact@xumoyun.uz>
2025-12-18 00:32:40 +0500

    add comment

--- 1023.js
+++ 1023.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
 const commands = lines[1];
+
 let left = 0;
 let right = 0;
 
@@ -9,4 +10,5 @@ for (const command of commands) {
     else right++;
 }
 
+// left va right oralig'idagi butun sonlarning soni (0ni ham hisobga olib)
 console.log(left + right + 1);
