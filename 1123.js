88698266fcc459a9814b5379827ad2dd215c7e34
Khumoyun <contact@xumoyun.uz>
2026-01-03 01:51:11 +0500

    add solutions

--- 1123.js
+++ 1123.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const [n, a, b] = fs.readFileSync(0, 'utf8').trim().split(' ').map(BigInt);
+const [n, a, b] = fs.readFileSync(0, 'utf8').trim().split(' ').map(Number);
 
-
-console.log(dry);
+const dry = n * (1 - a / 100);
+const newDry = 1 - b / 100;
+console.log(dry / newDry);
