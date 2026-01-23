674f69c3ddddffae591316403f5a0742bcb12eb2
Khumoyun <contact@xumoyun.uz>
2026-01-23 22:51:43 +0500

    format code

--- R054B.js
+++ R054B.js
@@ -4,6 +4,7 @@ const lines = fs.readFileSync(0, 'utf8').split('\n');
 for (let i = 2; i < lines.length; i += 2) {
     const line = lines[i].split(' ').map(Number);
     let count = 1;
+
     for (let i = 1; i < line.length; i++) 
         if (line[i] < line[i - 1]) count++;
 
