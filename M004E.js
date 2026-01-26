0e2f55acf25dff71b2e8ad54d15400d38a4ad13b
Khumoyun <contact@xumoyun.uz>
2026-01-26 16:49:13 +0500

    add solution

---
+++ M004E.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const lines = fs.readFileSync(0, 'utf8').split('\n');
+
+for (let i = 1; i < lines.length; i++) {
+    const n = +lines[i];
+    console.log(Math.ceil(n / 2));
+}
