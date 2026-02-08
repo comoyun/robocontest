ad9f42fc3a00b6edee4522bfd9e76b75aadb3d36
Khumoyun <contact@xumoyun.uz>
2026-02-08 09:33:56 +0500

    add solution

--- M014C.js
+++ M014C.js
@@ -1,2 +1,17 @@
 const fs = require('fs');
-const [a, b] = fs.readFileSync(0, 'utf8');
+const [a, b] = fs.readFileSync(0, 'utf8').split(' ').map(Number);
+
+// nechta yutish variantlari bor?
+// +1 chunki akalarini toshi bilan
+// bir xil bo'lib qolsa ham yutadi
+const have = 6 - Math.max(a, b) + 1;
+
+// a/b ifodani soddalashtirish uchun
+// gcd = ekub
+const gcd = (a, b) => {
+    if (b === 0) return a;
+    return gcd(b, a % b);
+};
+
+const f = gcd(have, 6);
+console.log(have / f + '/' + 6 / f);
