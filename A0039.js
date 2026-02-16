e27f4e27b9fac82a4fc7d0febeb0ffac4e538c89
Khumoyun <contact@xumoyun.uz>
2026-02-16 23:04:19 +0500

    add explanation

--- A0039.js
+++ A0039.js
@@ -4,9 +4,14 @@ const [n, k] = lines[0].split(' ').map(Number);
 const nums = lines[1].split(' ').map(Number);
 
 nums.sort((a, b) => {
+    // dastlab qoldig'iga qarab saralaymiz
+    // kam qoldiqli element oldin joylashtiriladi
     const da = a % k;
     const db = b % k;
 
+    // agar qoldiqlar teng bo'lsa
+    // a va bni katta kichikligiga qarab
+    // saralaymiz
     if (da === db) return a - b;
     return db - da;
 });
