a91e2be49a53a379e80f0e9d39e3c6b717cbfca5
Khumoyun <contact@xumoyun.uz>
2026-02-28 01:21:33 +0500

    add solution

---
+++ A0052.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
+
+for (let i = 2; i < lines.length; i += 3) {
+    const nums = lines[i].trim().split(' ').map(Number);
+    const k = +lines[i + 1];
+
+    nums.sort((a, b) => a - b);
+    let sum = 0;
+    let result = 0;
+
+    for (let i = 0; i < k; i++) {
+        const num = nums[i];
+        const count = (num - sum) * (nums.length - i);
+        result += count;
+        sum += num;
+    }
+
+    console.log(result);
+}
