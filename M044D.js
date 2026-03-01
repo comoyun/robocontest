8988a795daa1a0283fa72ca98f1cece8117330fe
Khumoyun <contact@xumoyun.uz>
2026-03-01 14:59:18 +0500

    add solution

---
+++ M044D.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const str = fs.readFileSync(0, 'utf8').trim();
+
+const map = { A: 0, B: 0, C: 0 };
+
+for (const char of str) {
+    if (char === 'C') {
+        if (map.B === 0) {
+            console.log('NO');
+            process.exit(0);
+        }
+        map.B--;
+    }
+
+    if (char === 'B') {
+        if (map.A === 0) {
+            console.log('NO');
+            process.exit(0);
+        }
+        map.A--;
+    }
+
+    map[char]++;
+}
+
+console.log(map.A + map.B === 0 ? 'YES' : 'NO');
