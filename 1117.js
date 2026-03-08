7cda4912969ef8723aa058e8beb25980251ae003
Khumoyun <contact@xumoyun.uz>
2026-03-08 11:35:51 +0500

    add explanation

--- 1117.js
+++ 1117.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
 const [a, b, c] = fs.readFileSync(0, 'utf8').trim().split(' ').map(Number);
 
+// a-b va b–c orasidagi kattaroq bo'sh joyni chiqaradi
 console.log(Math.max(b - a - 1, c - b - 1));
