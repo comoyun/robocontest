3cf7b42e705f745dd414dce14dca84ad05658b1f
Khumoyun <contact@xumoyun.uz>
2026-03-24 21:57:35 +0500

    add explanation

--- M108A.js
+++ M108A.js
@@ -3,13 +3,19 @@ const lines = fs.readFileSync(0, 'utf8').trim().split('\n');
 const ages = lines[1].trim().split(' ').map(Number);
 
 const groups = [0, 0, 0, 0];
+
+// yoshni guruh indeksiga ajratadi
+// keyin sanash oson bo'ladi
 const getGroup = age => {
     if (age < 19) return 0;
-    else if (age < 36) return 1;
-    else if (age < 61) return 2;
+    if (age < 36) return 1;
+    if (age < 61) return 2;
     return 3;
 };
 
+// chastotasini hisoblaymiz
 for (const age of ages) groups[getGroup(age)]++;
 
+// massiv elementlarini orasiga bo'shliq
+// qo'ygan holda ekranga chiqaradi
 console.log(groups.join(' '));
