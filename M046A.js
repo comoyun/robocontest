86839e1450bbef0c4c06267e44a5cb54440f9a72
Khumoyun <contact@xumoyun.uz>
2026-04-09 21:10:34 +0500

    add explanation

--- M046A.js
+++ M046A.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
 const n = +fs.readFileSync(0, 'utf8').trim();
 
+// bittasi k ta olsa (1 ≤ k ≤ n-1), ikkinchisi n-k oladi 
+// jami n-1 ta usulda bo'lish mumkin
 console.log(n - 1);
