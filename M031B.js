8a3367a0d8506cf16dae9bafa4c082b704725984
Khumoyun <contact@xumoyun.uz>
2026-06-02 23:18:57 +0500

    add solution

--- M031B.js
+++ M031B.js
@@ -14,3 +14,34 @@ for (let i = 0; i < 4; i++) {
 }
 
 console.log('No');
+
+/*
+
+Backtracking usuli bilan yechimni universal qilish mumkin (N ta qatnashuvchi uchun).
+
+const fs = require('fs');
+const skills = fs.readFileSync(0, 'utf8').trim().split(' ').map(Number);
+let total = skills.reduce((prev, curr) => prev + curr, 0);
+
+if (total & 1) {
+    console.log('No');
+    process.exit(0);
+}
+
+const backtrack = (i, sum, count) => {
+    if (i >= skills.length) return;
+    if (count === 3) { // yoki N / 2
+        if (sum === total / 2) {
+            console.log('Yes');
+            process.exit(0);
+        }
+        return;
+    }
+    backtrack(i + 1, sum, count);
+    backtrack(i + 1, sum + skills[i], count + 1);
+};
+
+backtrack(0, 0, 0);
+
+console.log('No');
+*/
